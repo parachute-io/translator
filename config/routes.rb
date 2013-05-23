83aa20df6690c9690eba060c3e15c3e5511f6f53
pmartens <pieter@cg.nl>
2013-05-23 11:20:42 +0200

    enable translator in production

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 # When Rails >= 3.1
 if defined?(Translator::Engine)
-  if Rails.env != "production"
-    Translator::Engine.routes.draw do
-      resources :translations
-    end
+  Translator::Engine.routes.draw do
+    resources :translations
   end
 else
   Rails.application.routes.draw do
